abe054c4ee2403cd2ba5fbfd9c0bd31dcc75823a
Ubuntu <ubuntu@ip-172-31-16-105.ap-northeast-1.compute.internal>
2025-10-07 13:17:20 +0000

    課題提出4回目

--- resources/views/completed_list.blade.php
+++ resources/views/completed_list.blade.php
@@ -5,51 +5,55 @@
 
 {{-- メインコンテンツ --}}
 @section('contents')
-   @if ($errors->any())
-            <div>
-            @foreach ($errors->all() as $error)
-                {{ $error }}<br>
-            @endforeach
-            </div>
-        @endif
+@if ($errors->any())
+<div>
+    @foreach ($errors->all() as $error)
+    {{ $error }}<br>
+    @endforeach
+</div>
+@endif
 
 <h1>完了タスクの一覧</h1>
-<a href="/task/list">タスク一覧へ戻る</a>
-        <table border="1">
-            <tr>
-                <th>タスク名</th>
-                <th>期限</th>
-                <th>重要度</th>
-                <th>完了日時</th>
-            </tr>
- @foreach ($completed_list as $completedTask)
-        <tr>
-            <td>{{ $completedTask->name }}
-            <td>{{ $completedTask->period }}
-            <td>{{ $completedTask->getPriorityString() }}
-            <td>{{ $completedTask->created_at }}
+<a href="/task/list">タスク一覧に戻る</a>
+<table border="1">
+    <tr>
+        <th>タスク名</th>
+        <th>期限</th>
+        <th>重要度</th>
+        <th>タスク終了日</th>
+    </tr>
+    @foreach ($completed_list as $completedTask)
+    <tr>
+        <td>{{ $completedTask->name }}
+        <td>{{ $completedTask->period }}
+        <td>{{ $completedTask->getPriorityString() }}
+        <td>{{ $completedTask->created_at }}
 
-        @endforeach
-        </table>
-        <!-- ページネーション -->
-        {{-- {{ $completed_list->links() }} --}}
-        現在 {{ $completed_list->currentPage() }} ページ目<br>
-        @if ($completed_list->onFirstPage() === false)
-        <a href="/completed_tasks/list">最初のページ</a>
-        @else
-        最初のページ
-        @endif
-        / 
-        @if ($completed_list->previousPageUrl() !== null)
-            <a href="{{ $completed_list->previousPageUrl() }}">前に戻る</a>
-        @else
-            前に戻る
-        @endif
-        / 
-        @if ($completed_list->nextPageUrl() !== null)
-            <a href="{{ $completed_list->nextPageUrl() }}">次に進む</a>
-        @else
-            次に進む
-        @endif
-
+            @endforeach
+</table>
+<!-- ページネーション -->
+{{-- {{ $completed_list->links() }} --}}
+現在 {{ $completed_list->currentPage() }} ページ目<br>
+@if ($completed_list->onFirstPage() === false)
+<a href="/completed_tasks/list">最初のページ</a>
+@else
+最初のページ
+@endif
+/
+@if ($completed_list->previousPageUrl() !== null)
+<a href="{{ $completed_list->previousPageUrl() }}">前に戻る</a>
+@else
+前に戻る
+@endif
+/
+@if ($completed_list->nextPageUrl() !== null)
+<a href="{{ $completed_list->nextPageUrl() }}">次に進む</a>
+@else
+次に進む
+@endif
+<br>
+<hr>
+<menu label="リンク">
+    <a href="/logout">ログアウト</a><br>
+</menu>
 @endsection
